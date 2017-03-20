c3c9c55ea6ff21afd29f18b3cd3c144d96810961
Mikal Villa <mikal.villa@knowit.no>
2017-03-20 23:34:00 +0100

    Better reading now

--- ansible/roles/ctfserver/files/levels/level00/service/xss-bot-run.sh
+++ ansible/roles/ctfserver/files/levels/level00/service/xss-bot-run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 while sleep 1;
-	do phantomjs --ignore-ssl-errors=true --local-to-remote-url-access=true --web-security=false --ssl-protocol=any xss-bot.js;
+	do phantomjs --ignore-ssl-errors=true \
+      --local-to-remote-url-access=true \
+      --web-security=false \
+      --ssl-protocol=any xss-bot.js;
 done;
