545a5e366b9026cbf3b578cbfb938e9bc09d1548
Mikal Villa <mikal.villa@knowit.no>
2017-03-25 22:16:15 +0100

    Bigbrother scripts updates

--- ansible/roles/ctfserver/files/bigbrother/list-active-ssh-connections.sh
+++ ansible/roles/ctfserver/files/bigbrother/list-active-ssh-connections.sh
@@ -1,24 +1,24 @@
  #!/usr/bin/bash
 
- function getSessionHashFromSSH_CLIENT() {
-   ipanddestport=`/usr/bin/echo $SSH_CLIENT | /usr/bin/awk '{ printf "%s:%d", $1, $2 }'`
-   sha256hash=`/usr/bin/echo $ipanddestport | /usr/bin/sha256sum | /usr/bin/awk '{ print $1 }'`
-   sessionhash=`/usr/bin/echo $sha256hash | /usr/bin/head -c 7`
-   export SESSION_HASH_ID=$sessionhash
- }
+function getSessionHashFromSSH_CLIENT() {
+  ipanddestport=`/usr/bin/echo $SSH_CLIENT | /usr/bin/awk '{ printf "%s:%d", $1, $2 }'`
+  sha256hash=`/usr/bin/echo $ipanddestport | /usr/bin/sha256sum | /usr/bin/awk '{ print $1 }'`
+  sessionhash=`/usr/bin/echo $sha256hash | /usr/bin/head -c 7`
+  export SESSION_HASH_ID=$sessionhash
+}
 
- function getSessionHashFromNetstat() {
-   ipanddestport=$1
-   sha256hash=`/usr/bin/echo $ipanddestport | /usr/bin/sha256sum | /usr/bin/awk '{ print $1 }'`
-   sessionhash=`/usr/bin/echo $sha256hash | /usr/bin/head -c 7`
-   export SESSION_HASH_ID=$sessionhash
-   echo $sessionhash
- }
+function getSessionHashFromNetstat() {
+  ipanddestport=$1
+  sha256hash=`/usr/bin/echo $ipanddestport | /usr/bin/sha256sum | /usr/bin/awk '{ print $1 }'`
+  sessionhash=`/usr/bin/echo $sha256hash | /usr/bin/head -c 7`
+  export SESSION_HASH_ID=$sessionhash
+  echo $sessionhash
+}
 
- export CURRENT_CONNECTIONS=`netstat -tnpa | grep 'ESTABLISHED.*sshd' | awk '{ print $5 }'`
- export CURRENT_SSH_CONN_HASHES=""
+export CURRENT_CONNECTIONS=`netstat -tnpa | grep 'ESTABLISHED.*sshd' | awk '{ print $5 }'`
+export CURRENT_SSH_CONN_HASHES=""
 
 
- for connection in $CURRENT_CONNECTIONS; do
-   echo "$connection $(getSessionHashFromNetstat $connection)";
- done
+for connection in $CURRENT_CONNECTIONS; do
+  echo "$connection $(getSessionHashFromNetstat $connection)";
+done
