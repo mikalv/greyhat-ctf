9f42e135277acf4abcedf0f8a13ae8042873f20a
Mikal Villa <mikal.villa@knowit.no>
2017-04-06 15:28:28 +0200

    Misc improvements

--- ansible/roles/ctfserver/files/levels/level00/level00_services/websrv/iframes/make_app.py
+++ ansible/roles/ctfserver/files/levels/level00/level00_services/websrv/iframes/make_app.py
@@ -1,13 +1,71 @@
 from werkzeug.wsgi import SharedDataMiddleware
+from werkzeug.routing import Map, Rule
+from werkzeug.wrappers import Request, Response
+from werkzeug.exceptions import HTTPException, NotFound
 import os, sys
+
+SESSION_DISCOVERY_DIR='/var/ctfgame1/tmp/sessions/'
+SESSION_WRITE_DIR=''
+
+def getActiveSessions():
+  listen = []
+  for fn in os.listdir(SESSION_DISCOVERY_DIR):
+    if os.path.isfile(os.path.join(SESSION_DISCOVERY_DIR,fn)):
+      listen.append(fn)
+  return listen
+
+def renderIFrameHell(sessionList):
+  def getIframe(sessionId):
+    return """ <iframe src="%s"></iframe><br>\n """ % (sessionId,)
+
+  iframesList = []
+  for sessionId in getActiveSessions():
+    if os.path.exists(os.path.join(SESSION_WRITE_DIR, sessionId)):
+      if os.path.exists(os.path.join(SESSION_WRITE_DIR, sessionId, 'workspace', 'inject.html')):
+        iframesList.append(getIframe(sessionId))
+
+  htmlData = """
+    <html>
+      <head>
+        <title>IFrames!</title>
+      </head>
+      <body>
+        %s
+      </body>
+    </html>
+    """ % ("\n".join( iframesList ) ,)
+
 
 class Level00Website(object):
 
   def __init__(self, config):
     print("[+] Level 00 Website up and running")
+    self.url_map = Map([
+      Rule('/', endpoint='index'),
+      Rule('/<sessionId>', endpoint='new_iframe'),
+    ])
+
+  def on_new_iframe(self, request, sessionId):
+    iframePath = os.path.join(SESSION_WRITE_DIR, sessionId, 'workspace', 'inject.html')
+    textString = ''
+    if os.path.isfile(iframePath):
+      f = open(os.path.join(iframePath, ''))
+      textString = f.read()
+      f.close()
+    else:
+      textString = 'none'
+    return Response('%s' % (request,), mimetype='text/html')
+
+  def on_index(self, request):
+    return Response('<b>hei</b>', mimetype='text/html')
 
   def dispatch_request(self, request):
-    return Response('Hello World!')
+    adapter = self.url_map.bind_to_environ(request.environ)
+    try:
+      endpoint, values = adapter.match()
+      return getattr(self, 'on_' + endpoint)(request, **values)
+    except HTTPException, e:
+      return e
 
   def wsgi_app(self, environ, start_response):
     request = Request(environ)
